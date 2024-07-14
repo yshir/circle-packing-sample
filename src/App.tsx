ca7d1a1dbeb3cb2fef8e3e3eaca9d5c3e966cde2
yshir <yshir0020@gmail.com>
2024-07-15 00:15:04 +0900

    feat(App.tsx): integrate D3.js to visualize hierarchical data
    Add D3.js and its type definitions to dependencies. Implement a basic
    hierarchical data visualization using D3.js in the App component. This
    enhancement allows for dynamic and interactive data representation,
    improving the application's data visualization capabilities.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,80 @@
+import { useEffect, useRef } from 'react';
+import * as d3 from 'd3';
+
+const WIDTH = 800;
+const HEIGHT = 600;
+
+type Node = {
+  name: string;
+  value: number;
+  children?: Node[];
+};
+
+const data = {
+  name: 'A',
+  children: [
+    { name: 'B', value: 25 },
+    {
+      name: 'C',
+      children: [
+        { name: 'D', value: 10 },
+        { name: 'E', value: 15 },
+        { name: 'F', value: 10 },
+      ],
+    },
+    { name: 'G', value: 15 },
+    {
+      name: 'H',
+      children: [
+        { name: 'I', value: 20 },
+        { name: 'J', value: 10 },
+        {
+          name: 'K',
+          children: [
+            { name: 'L', value: 5 },
+            { name: 'M', value: 15 },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
 export function App() {
-  return 'Hello';
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const root = d3.pack<Node>().size([WIDTH, HEIGHT]).padding(0)(
+      d3
+        .hierarchy<Node>(data as Node) // value filled after calling sum()
+        .sum((d) => d.value)
+    );
+
+    const node = d3
+      .select(ref.current)
+      .selectAll('.node')
+      .data(root.descendants())
+      .enter()
+      .append('g')
+      .attr('transform', (d) => `translate(${d.x},${d.y})`);
+
+    const color = ['orange', 'Khaki', 'Ivory'];
+    node
+      .append('circle')
+      .attr('r', (d) => d.r)
+      .attr('stroke', 'black')
+      .attr('fill', (d) => color[d.depth % color.length]);
+
+    node
+      .append('text')
+      .style('text-anchor', (d) => (d.children ? 'end' : 'middle'))
+      .attr('font-size', '150%')
+      .text((d) => (d.children ? '' : d.data.name));
+  }, []);
+
+  return (
+    <div>
+      <svg width={WIDTH} height={HEIGHT} ref={ref} />
+    </div>
+  );
 }
